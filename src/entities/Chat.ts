0e843e298c0e3bd2dcd54ab452f76ccfbfce3e46
Kwangho <khseok1060@gmail.com>
2018-07-28 18:28:48 +0900

    #1.22 Chat and Message Entities part One

---
+++ src/entities/Chat.ts
@@ -0,0 +1,18 @@
+import { BaseEntity, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Message from "./Message";
+
+@Entity()
+class Chat extends BaseEntity {
+
+  @PrimaryGeneratedColumn() id: number;
+
+  @OneToMany(type => Message, message => message.chat)
+  messages: Message[];
+
+  @CreateDateColumn() createdAt: string;
+  
+  @UpdateDateColumn() upadtedAt: string;
+
+}
+
+export default Chat;
