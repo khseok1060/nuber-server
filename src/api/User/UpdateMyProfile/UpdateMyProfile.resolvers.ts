336b9c7e42ca8603c13c81e4b348216a8ccbc895
Kwangho <khseok1060@gmail.com>
2018-08-05 11:45:05 +0900

    #1.57.1 UpdateMyProfile Resolver Bug Fixing

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,29 +1,31 @@
 import User from "../../../entities/User";
 import { UpdateMyProfileMutationArgs, UpdateMyProfileResponse} from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
+import cleanNullArgs from "../../../utils/cleanNullArgs";
 import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
   Mutation: {
     UpdateMyProfile: privateResolver(async(_, args: UpdateMyProfileMutationArgs, { req }): Promise<UpdateMyProfileResponse> => {
       const user: User = req.user;
-      const notNull = {};
-      Object.keys(args).forEach(key => {
-        if (args[key] !== null) {
-          notNull[key] = args[key];
-        }
-      });
+      const notNull:any = cleanNullArgs(args); // 👈🏻 Add ':any'
+      // ⚠️ Take the if(notNull.password) OUT of the try/catch
+      if(notNull.password !== null) { // 👈🏻 Change from args to notNull
+        user.password = notNull.password; // 👈🏻 Same here
+        user.save();
+        delete notNull.password; // <--  ⚠️⚠️⚠️ Delete password  from notNull or is going to be saved again without encoding ⚠️⚠️⚠️
+      }
       try {
-        await User.update({ id: user.id }, { ...notNull });
+        await User.update({ id: user.id }, { ...notNull }); 
         return {
           ok: true,
           error: null
-        }
-      } catch(error) {
+        };
+      } catch (error) {
         return {
           ok: false,
           error: error.message
-        }
+        };
       }
     })
   }
