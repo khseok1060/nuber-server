d70561477ad78abd9716ec2f5a3eee1b39a3c2c4
Kwangho <khseok1060@gmail.com>
2018-08-05 11:34:42 +0900

    #1.57 UpdateMyProfile Resolver part Two

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -7,7 +7,24 @@ const resolvers: Resolvers = {
   Mutation: {
     UpdateMyProfile: privateResolver(async(_, args: UpdateMyProfileMutationArgs, { req }): Promise<UpdateMyProfileResponse> => {
       const user: User = req.user;
-      await User.update({ id: user.id }, { ...args })
+      const notNull = {};
+      Object.keys(args).forEach(key => {
+        if (args[key] !== null) {
+          notNull[key] = args[key];
+        }
+      });
+      try {
+        await User.update({ id: user.id }, { ...notNull });
+        return {
+          ok: true,
+          error: null
+        }
+      } catch(error) {
+        return {
+          ok: false,
+          error: error.message
+        }
+      }
     })
   }
 }
