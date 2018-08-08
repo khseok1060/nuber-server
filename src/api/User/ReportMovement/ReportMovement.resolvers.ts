2e14e190e6bf99b3ba15221a76b7f8cba6511f2c
Kwangho <khseok1060@gmail.com>
2018-08-08 20:05:55 +0900

    #1.67 DriversSubscription part Two

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -6,11 +6,12 @@ import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
   Mutation: {
-    ReportMovement: privateResolver(async(_, args: ReportMovementMutationArgs, { req }): Promise<ReportMovementResponse> => {
+    ReportMovement: privateResolver(async(_, args: ReportMovementMutationArgs, { req, pubSub }): Promise<ReportMovementResponse> => {
       const user: User = req.user;
       const notNull = cleanNullArgs(args);
       try {
         await User.update({ id: user.id }, { ...notNull });
+        pubSub.publish("driverUpdate", { DriversSubscription: user });
         return {
           ok: true,
           error: null
