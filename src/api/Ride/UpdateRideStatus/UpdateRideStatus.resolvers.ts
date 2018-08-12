fef5a41e22d577bc17d4c52a39c170a39739d695
Kwangho <khseok1060@gmail.com>
2018-08-12 12:45:12 +0900

    #1.77 UpdateRideStatus Resolver part Two

--- src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -18,25 +18,46 @@ const resolvers: Resolvers = {
         const user: User = req.user;
         if (user.isDriving) {
           try {
-            const ride = await Ride.findOne({
-              id: args.rideId,
-              status: "REQUESTING"
-            });
+            let ride: Ride | undefined;
+            if (args.status === "ACCEPTED") {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                status: "REQUESTING"
+              });
+              if (ride) {
+                ride.driver = user;
+                user.isTaken = true;
+                user.save();
+              }
+            } else {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                driver: user
+              });
+            }
             if (ride) {
               ride.status = args.status;
               ride.save();
-              
+              return {
+                ok: true,
+                error: null
+              };
             } else {
               return {
                 ok: false,
                 error: "Can't update ride"
-              }
+              };
             }
-          } catch(error) {
+          } catch (error) {
             return {
               ok: false,
               error: error.message
-            }
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "You are not driving"
           }
         }
       }
