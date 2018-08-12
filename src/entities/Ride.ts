d600ab3738a7d2c936ff5ef5387c662b5243e597
Kwangho <khseok1060@gmail.com>
2018-08-12 11:21:38 +0900

    #1.72 RequestRide Resolver

--- src/entities/Ride.ts
+++ src/entities/Ride.ts
@@ -1,55 +1,62 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
 import { rideStatus } from "../types/types";
 import User from "./User";
 
 @Entity()
 class Ride extends BaseEntity {
-
   @PrimaryGeneratedColumn() id: number;
 
   @Column({
-      type: "text", 
-      enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"]
+    type: "text",
+    enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"],
+    default: "ACCEPTED"
   })
   status: rideStatus;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   pickUpAddress: string;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   pickUpLat: number;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   pickUpLng: number;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   dropOffAddress: string;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   dropOffLat: number;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   dropOffLng: number;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   price: number;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   distance: string;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   duration: string;
 
   @ManyToOne(type => User, user => user.ridesAsPassenger)
   passenger: User;
 
-  @ManyToOne(type => User, user => user.ridesAsDriver)
+  @ManyToOne(type => User, user => user.ridesAsDriver, { nullable: true })
   driver: User;
 
   @CreateDateColumn() createdAt: string;
-  
-  @UpdateDateColumn() updatedAt: string;
 
+  @UpdateDateColumn() updatedAt: string;
 }
 
 export default Ride;
